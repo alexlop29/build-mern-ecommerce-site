80911a05206caf53ff131e4b72343f880589ccfd
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-03 16:59:21 -0400

    Update styled-toolbar.ts

--- front-end/src/modules/navigation-bar/atoms/styled-toolbar.ts
+++ front-end/src/modules/navigation-bar/atoms/styled-toolbar.ts
@@ -1,9 +1,6 @@
 import { Toolbar } from '@mui/material';
 import styled from '@emotion/styled';
 
-
-// NOTE: (Alopez) Observation; It only wraps the ProShop Logo and Nav Bar
-// Components. 
 const StyledToolbar = styled(Toolbar)`
   background: #EEEE02;
   color: black;
