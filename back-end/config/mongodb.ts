6a82226e5b3854707c6cd164cfbfaa15099051d9
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-03 16:57:36 -0400

    Update mongodb.ts

--- back-end/config/mongodb.ts
+++ back-end/config/mongodb.ts
@@ -1,9 +1,7 @@
 import { MONGO_DB_URI } from './environment';
 
-// NOTE: (alopez) Lack of documentation on dynamic imports; Update in a future release.
 const mongoose = require('mongoose');
 
-// NOTE: (alopez) Consider using Sentry to track errors in connecting to the database.
 try {
   const conn = mongoose.connect(MONGO_DB_URI);
 } 
