9b2145c5598d810bc9428857cf36ac524afce610
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-03 16:58:59 -0400

    Update styled-container.ts

--- front-end/src/modules/navigation-bar/atoms/styled-container.ts
+++ front-end/src/modules/navigation-bar/atoms/styled-container.ts
@@ -1,7 +1,6 @@
 import { Container } from '@mui/material';
 import styled from '@emotion/styled';
 
-// NOTE: (Alopez) Observation; It only wraps around the ProShop logo.
 const StyledContainer = styled(Container)`
   bakground: white;
 `;
