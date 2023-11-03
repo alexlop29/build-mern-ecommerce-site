26e6078228dd4585350f33941afbe780a5d447c0
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-03 17:00:35 -0400

    Update styled-title.ts

--- front-end/src/modules/shared/atoms/styled-title.ts
+++ front-end/src/modules/shared/atoms/styled-title.ts
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 
-// Step 2: Get the Import URL from Google Fonts and add it here
 const Title = styled.h1`
   @import url('https://fonts.googleapis.com/css2?family=Shrikhand&display=swap');
   font-family: 'Shrikhand', cursive;
