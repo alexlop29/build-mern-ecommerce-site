34aed38c35d0973fe3ceeb4ad7f3ed6cf84f9fef
Alexander Lopez <ClarkAlexander10@gmail.com>
2023-11-03 16:58:48 -0400

    Update styled-box.tsx

--- front-end/src/modules/navigation-bar/atoms/styled-box.tsx
+++ front-end/src/modules/navigation-bar/atoms/styled-box.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@mui/material';
 import styled from '@emotion/styled';
 
-// NOTE: (alopez) Wraps around the elements in the nav bar, but leaves spacing on
-// the side; Filled by nav bar.
 const StyledBox = styled(Box)`
   background: #EEEE02;
   height: 70px;
